6ebefb3790993397dafeabaca389d60082a2f6dd
August Shah <augustshah@02pilot.com>
2021-04-15 21:11:53 -0400

    Minor fix

--- src/Globals.js
+++
@@ -1,9 +0,0 @@
-
-
-export const remoteConfigDefaults ={
-    REACT_APP_ServiceId: 'service_tmltttc',
-    REACT_APP_TemplateId: 'template_664z6ho',
-    REACT_APP_UserId: 'user_H2dSH0v7XWD3nG6vyyosk',
-};
-
-
