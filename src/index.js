236978698099decd1372023d3783768088389ec6
August Shah <augustshah@02pilot.com>
2021-08-25 18:32:11 -0400

    Firebase configured

--- src/index.js
+++ src/index.js
@@ -8,7 +8,20 @@ import './index.css';
 import Header from './includes/header.js';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import firebase from "firebase/app";
 
+var firebaseConfig = {
+  apiKey: `${process.env.REACT_APP_apiKey}`,
+  authDomain: `${process.env.REACT_APP_authDomain}`,
+  projectId: `${process.env.REACT_APP_projectId}`,
+  storageBucket: `${process.env.REACT_APP_storageBucket}`,
+  messagingSenderId: `${process.env.REACT_APP_messagingSenderId}`,
+  appId: `${process.env.REACT_APP_appId}`,
+  measurementId: `${process.env.REACT_APP_measurementId}`
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 
 
