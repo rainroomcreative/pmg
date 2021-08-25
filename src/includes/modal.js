b38bfa4c4b6212b9e441d86d312e1088e09db068
August Shah <augustshah@02pilot.com>
2021-08-25 09:03:55 -0400

    EmailJS, UI, and Recaptcha Configured

--- src/includes/modal.js
+++ src/includes/modal.js
@@ -1,5 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import emailjs from 'emailjs-com';
+import{ init } from 'emailjs-com';
+import ReCAPTCHA from "react-google-recaptcha";
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
 
 export default function Modal() {
     const Modalclase =()=>{
@@ -8,16 +19,49 @@ export default function Modal() {
         document.body.style.overflow="auto"; 
         document.body.classList.remove('modal-open');
      };
-     function sendEmail(e) {
-        e.preventDefault();
+
+     init(`${process.env.REACT_APP_UserId}`);
+     const [open, setOpen] = useState(false);
+     const [verified, setVerified] = useState(false)
+
+     const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
     
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
-          .then((result) => {
-              console.log(result.text);
-          }, (error) => {
-              console.log(error.text);
-          });
+        setOpen(false);
+      };
+      // function that sets the verfied state to true
+      const handleVerify = () => {
+        
+        setVerified(true);
+      
       }
+
+     const sendEmail = (e)  => {
+
+        if(verified) {
+
+          setOpen(true);
+        
+          e.preventDefault();
+      
+          emailjs.sendForm(
+            `${process.env.REACT_APP_ServiceId}`,
+            `${process.env.REACT_APP_TemplateId}`, 
+            e.target, 
+            `${process.env.REACT_APP_UserId}`)
+            .then((result) => {
+                console.log(result);
+            }, (error) => {
+                console.log(error);
+            });
+        } else {
+
+          return;
+    }
+  }
+
     return (
     <div className="modal fade customodal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
             aria-hidden="true">
@@ -28,52 +72,60 @@ export default function Modal() {
                         <button type="button"  onClick={Modalclase} id="btnclose" className="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
-                        <h4><span>Contact Us</span> Lorem Ipsum is simply dummy text</h4>
+                        <h4><span>Contact Us</span> Ready to elevate your business?</h4>
                         <form className="contact-form" onSubmit={sendEmail}>
                             <div className="row">
                             <div className="col-md-6">
                                 <div className="form-outline">
                                 <i className="fa fa-user prefix"></i>
-                                <input type="text" className="form-control" name="fname" id="firstname"  required/>
+                                <input type="text" className="form-control" name="first_name" id="firstname"  required/>
                                 <label className="form-label" for="firstname">First Name</label>
                                 </div>
                             </div>
                             <div className="col-md-6">
                                 <div className="form-outline">
                                 <i className="fa fa-user prefix"></i>
-                                <input type="text" className="form-control" name="lname" id="lastname" required/>
+                                <input type="text" className="form-control" name="last_name" id="lastname" required/>
                                 <label className="form-label" for="lastname">Last Name</label>
                                 </div>
                             </div>
                             <div className="col-md-6">
                                 <div className="form-outline">
                                 <i className="fa fa-envelope prefix"></i>
-                                <input type="email" className="form-control" name="lemail" id="emailad" required/>
+                                <input type="email" className="form-control" name="email" id="emailad" required/>
                                 <label className="form-label" for="emailad">Email Address</label>
                                 </div>
                             </div>
                             <div className="col-md-6">
                                 <div className="form-outline">
                                 <i className="fa fa-phone prefix"></i>
-                                <input type="number" className="form-control" name="lphone" id="typePhone" required/>
+                                <input type="number" className="form-control" name="phone_number" id="typePhone"/>
                                 <label className="form-label" for="typePhone">Phone No.</label>
                                 </div>
                             </div>
                             <div className="col-md-12">
                                 <div className="form-outline ah_textbox">
                                 <i className="fa fa-envelope-open-text prefix"></i>
-                                <textarea className="form-control" id="typemasage" name="lmessage" rows="4" required></textarea>
+                                <textarea className="form-control" id="typemasage" name="message" rows="4" required></textarea>
                                 <label className="form-label" for="typemasage">Message</label>
                                 </div>
                             </div>
                             </div>
                             <div className="col-md-12 text-center">
+                            <ReCAPTCHA
+                                sitekey={`${process.env.REACT_APP_sitekey}`}
+                                onChange={handleVerify}
+                            />
                             <button type="submit" className="btn btn-primary">Submit Now</button>
                             </div>
                         </form>
                         </div> 
                         <div className="modal-footer">
-                        
+                        <Snackbar open={open} autoHideDuration={4000} onClose={handleClose}>
+                            <Alert onClose={handleClose} severity="info">
+                                Your Email has been sent 👍
+                            </Alert>
+                        </Snackbar>  
                         </div>
                     </div>
                     </div>
