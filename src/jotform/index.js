bf89aafdc8b9d6fb6526afe9b55ce61b2bab3b36
August Shah <augustshah@02pilot.com>
2021-09-11 20:38:19 -0400

    Jira ID: DS-133
    Problem:
    Wanted to streamline the intake process with new clients and besides sharing an onboarding form link, wanted to integrate the form into the website
    
    Solution:
    * Installed the Jotform npm package that allows forms to be easily added into React
    * Added padding to the form so that it wasnt hugging the header
    * Added password protection the route (test in PROD)
    
    Note
    Before merging with develop run integration tests

---
+++ src/jotform/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import JotformEmbed from 'react-jotform-embed';
+
+
+
+export default function Index() {
+
+ 
+    return(
+        <div className="pt-5">
+           <JotformEmbed src="https://form.jotform.com/212536409025046" />
+        </div>
+    )
+}
