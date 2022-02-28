61d8131cc0908313d0c64f93c2e373634f8bb990
August Shah <augustshah@02pilot.com>
2022-02-27 20:07:06 -0500

    Made portfolio works dynamic

--- src/portfolio/our-work.js
+++ src/portfolio/our-work.js
@@ -19,16 +19,16 @@ export default function Ourwork() {
         const {portfolios} = await request(
           `https://api-us-east-1.graphcms.com/v2/${process.env.REACT_APP_GraphCMSKey}/master`,
           `
-         {
-            portfolios(stage: PUBLISHED, locales: [en]) {
-                title
-                description
-                image {
-                  url
-                }
-                link
+          {
+            portfolios(stage: PUBLISHED, locales: [en], orderBy: createdAt_DESC) {
+              title
+              description
+              image {
+                url
               }
-         }
+              link
+            }
+          }
       `
         )
          setWorks(portfolios);
@@ -45,113 +45,81 @@ export default function Ourwork() {
       }
  
 
+   
+      // The below function gets the even and odd indexes of the array and saves it to a new Array. 
+      var even = [];
+     const dynamicArray = (a) => {
+        for (var i = 0; i < a.length; i++) {
+            if(i % 2 === 0) { // index is even
+                even.push(a[i]);
+            } else {
+                even.push(a[i]);
+            }
+        }
+        return works
+     }
+     dynamicArray(works);
+     // Mapping over the new array and creating new UI elements.
+    const dynamicWorks = even.map((item, index) => {
+        if(index % 2 === 0) {
+            return(
+                <section className="banner our-work bg-gray">
+                <div className="container">
+                    <div className="row work-1">
+                        <div className="col-md-6">
+                            <figure className="hover-overlay ripple ripple-surface ripple-surface-light">
+                                <img src={item.image.url} alt="portfolio 1"/>
+                            </figure>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="box">
+                                <h4> {item.title} </h4>
+                                <p>{item.description}</p>
+                                {item.link ? 
+                                <ul>
+                                    <li><a target="_blank" href={item.link} rel="noreferrer">View Project</a></li>
+                                </ul>
+                                    : null}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            )
+        } else {
+            return (
+                <section className="banner our-work">
+                <div className="container">
+                    <div className="row work-2">
+                        <div className="col-md-6 order-md-1 order-2">
+                            <div className="box ">
+                                <h4>{item.title}</h4>
+                                <p>{item.description}</p>
+                                {item.link ? 
+                                <ul>
+                                    <li><a target="_blank" href={item.link} rel="noreferrer">View Project</a></li>
+                                </ul>
+                                    : null}
+                            </div>
+                        </div>
+                        <div className="col-md-6 order-md-2 order-1">
+                            <figure className="hover-overlay ripple ripple-surface ripple-surface-light">
+                                <img src={item.image.url} alt="portfolio 2" />
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            )
+        }
+  
+    })
 
-
-
-
-
+ 
+    
     return (
     <div>
-        <section className="banner our-work bg-gray">
-            <div className="container">
-                <div className="row work-1">
-                    <div className="col-md-6">
-                        <figure className="hover-overlay ripple ripple-surface ripple-surface-light">
-                            <img src={works[0].image.url} alt="portfolio 1"/>
-                        </figure>
-                    </div>
-                    <div className="col-md-6">
-                        <div className="box">
-                            <h4> {works[0].title} </h4>
-                            <p>{works[0].description}</p>
-                            <ul>
-                                <li><a target="_blank" href={works[0].link} rel="noreferrer">View Project</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section className="banner our-work">
-            <div className="container">
-                <div className="row work-2">
-                    <div className="col-md-6 order-md-1 order-2">
-                        <div className="box ">
-                            <h4>{works[1].title}</h4>
-                            <p>{works[1].description}</p>
-                            <ul>
-                                <li><a target="_blank" href={works[1].link} rel="noreferrer">View Project</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="col-md-6 order-md-2 order-1">
-                        <figure className="hover-overlay ripple ripple-surface ripple-surface-light">
-                            <img src={works[1].image.url} alt="portfolio 2" />
-                        </figure>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section className="banner our-work bg-gray mb-0">
-            <div className="container">
-                <div className="row work-3">
-                    <div className="col-md-6">
-                        <figure className="hover-overlay ripple ripple-surface ripple-surface-light">
-                            <img src={works[4].image.url} alt="portfolio 3" />
-                        </figure>
-                    </div>
-                    <div className="col-md-6">
-                        <div className="box">
-                            <h4>{works[4].title}</h4>
-                            <p>{works[4].description}</p>
-                            <ul>
-                                <li><a target="_blank" href={works[4].link} rel="noreferrer">View Project</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section className="banner our-work">
-            <div className="container">
-                <div className="row work-2">
-                    <div className="col-md-6 order-md-1 order-2">
-                        <div className="box ">
-                            <h4>{works[3].title}</h4>
-                            <p>{works[3].description}</p>
-                            <ul>
-                                <li><a target="_blank" href={works[3].link} rel="noreferrer">View Project</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="col-md-6 order-md-2 order-1">
-                        <figure className="hover-overlay ripple ripple-surface ripple-surface-light">
-                            <img src={works[3].image.url} alt="portfolio 2" />
-                        </figure>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section className="banner our-work bg-gray mb-0">
-            <div className="container">
-                <div className="row work-3">
-                    <div className="col-md-6">
-                        <figure className="hover-overlay ripple ripple-surface ripple-surface-light">
-                            <img src={works[2].image.url} alt="portfolio 3" />
-                        </figure>
-                    </div>
-                    <div className="col-md-6">
-                        <div className="box">
-                            <h4>{works[2].title}</h4>
-                            <p>{works[2].description}</p>
-                            <ul>
-                                <li><a target="_blank" href={works[2].link} rel="noreferrer">View Project</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        {dynamicWorks}
     </div>
     );
   }
