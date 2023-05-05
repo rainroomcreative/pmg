88434ac90dbc3d59d7328ab1cfb87d3592dc1a09
Josh Bosley <broskisworld@gmail.com>
2023-05-05 10:05:37 -0600

    faqs html fix

--- 02-v2/src/pricing/faqQuestion.js
+++ 02-v2/src/pricing/faqQuestion.js
@@ -8,10 +8,10 @@ const FaqItem = ({faq, onToggle, active}) => {
         <div className="faq lg:basis-[44%] lg:flex-initial">
             <div className={"pb-8 border-b-fadedBlack border-b-solid transition-all" + `${active ? " border-b-0" : " border-b"}`}>
                 <div className="flex flex-row justify-between items-center cursor-pointer" onClick={onToggle}>
-                    <h4 className="cursor-pointer">{question}</h4>
+                    <h4 className="cursor-pointer pointer-events-none">{question}</h4>
                     <Plus className={active? 'rotate-45 transition-all' : 'rotate-0 transition-all'}/>
                 </div> 
-                <div className={"font-normal mt-8 transition-all pointer-events-none" + `${active? ' block' : ' hidden'}`} dangerouslySetInnerHTML={answer}></div>
+                <div className={"font-normal mt-8 transition-all pointer-events-none" + `${active? ' block' : ' hidden'}`} dangerouslySetInnerHTML={{__html: answer}}></div>
             </div>
         </div> 
     )
